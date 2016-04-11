3dbd1a2a43814d5f6de489fe29b85b7132437bcc
Jared Roth <jared.roth@gmail.com>
2016-04-10 19:07:17 -0600

    Finish iteration 6
    Still holding off on iteration 4s 'your turn' section until finished because of time
    Mostly understanding things, but a lot of the concepts are hazy, like:

--- app/controllers/playlists_controller.rb
+++ app/controllers/playlists_controller.rb
@@ -1,4 +1,5 @@
 class PlaylistsController < ApplicationController
+  before_action :set_playlist, only: [:show, :edit, :update]
   def index
   end
 
@@ -8,16 +9,34 @@ class PlaylistsController < ApplicationController
 
   def create
     @playlist = Playlist.create(playlist_params)
-    redirect_to @playlist
+    if @playlist.save
+      redirect_to @playlist
+    else
+      render :new
+    end
   end
 
   def show
-    @playlist = Playlist.find(params[:id])
+  end
+
+  def edit
+  end
+
+  def update
+    if @playlist.update(playlist_params)
+      redirect_to playlist_path(@playlist)
+    else
+      render :edit
+    end
   end
 
   private
 
   def playlist_params
     params.require(:playlist).permit(:name, song_ids: [])
+  end
+
+  def set_playlist
+    @playlist = Playlist.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
     resources :songs, only: [:new, :create, :index, :destroy], controller: "artist/songs"
   end
   resources :songs, only: [:show, :edit, :update, :index, :destroy]
-  resources :playlists, only: [:index, :new, :create, :show]
+  resources :playlists, only: [:index, :new, :create, :show, :edit, :update]
 end
